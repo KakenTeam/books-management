5076e1f4a5dd4116c3465f36ce09399465cd77de
minhthuan274 <doanminhthuan274@gmail.com>
2018-11-28 11:32:29 +0700

    not seeds books

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name:  "Minh Thuan",
              activated: true,
              activated_at: Time.zone.now)
 
-99.times do |n|
+10.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -18,18 +18,18 @@ User.create!(name:  "Minh Thuan",
                activated_at: Time.zone.now)
 end
 
-100.times do |n|
-  title        = Faker::Book.title
-  author       = Faker::Book.author
-  publisher    = Faker::Book.publisher
-  quantity     = Faker::Number.between(1, 15)
-  availability = quantity
-  year         = Faker::Number.between(0, 2017)
-  Book.create!(title:      title,
-              author:     author,
-              publisher:  publisher,
-              quantity:   quantity,
-              availability: availability,
-              year:       year)
-end
+# 100.times do |n|
+#   title        = Faker::Book.title
+#   author       = Faker::Book.author
+#   publisher    = Faker::Book.publisher
+#   quantity     = Faker::Number.between(1, 15)
+#   availability = quantity
+#   year         = Faker::Number.between(0, 2017)
+#   Book.create!(title:      title,
+#               author:     author,
+#               publisher:  publisher,
+#               quantity:   quantity,
+#               availability: availability,
+#               year:       year)
+# end
 
