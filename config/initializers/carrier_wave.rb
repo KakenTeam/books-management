b0c44835e71632f63fab08acb30c35e1d81afeb4
minhthuan274 <doanminhthuan274@gmail.com>
2018-11-28 12:38:21 +0700

    setup env s3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,12 +6,12 @@ require 'carrierwave/storage/fog'
 if Rails.env.production?
     CarrierWave.configure do |config|
         config.fog_provider = 'fog/aws'
-        config.fog_directory     =  'books-management'
+        config.fog_directory     =  ENV['S3_BUCKET']
         config.fog_credentials = {
         # Configuration for Amazon S3
         :provider              => 'AWS',
-        :aws_access_key_id     => 'AKIAIPWNK3MNW6FH2OOQ',
-        :aws_secret_access_key => '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',
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
         :region                => 'ap-southeast-1'
         }
     end
