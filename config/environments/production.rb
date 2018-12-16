0461d18463db957e507e8cd66f613fa286772b53
minhthuan274 <doanminhthuan274@gmail.com>
2018-12-16 15:38:58 +0700

    config sendgrid mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,13 +88,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://books-management-kaken.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-     address:              'smtp.gmail.com',
-     port:                 587,
-     domain:               'books-management-kaken.herokuapp.com',
-     user_name:            'doanminhthuan274@gmail.com',
-     password:             'Binhthuan274',
-     authentication:       'plain',
-     enable_starttls_auto: true
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 end
