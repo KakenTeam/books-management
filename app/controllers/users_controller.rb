51ab804f255eb4730ebb12a0aa08421f0f0aab33
minhthuan274 <doanminhthuan274@gmail.com>
2018-12-16 10:50:30 +0700

    update edit info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy 
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.where(activated: true).paginate(page: params[:page],
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_mail 
+      @user.send_activation_mail
       flash[:info] = "Please check your email to activate you account."
       redirect_to root_url
     else
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy 
+    User.find(params[:id]).destroy
     flash[:success] = "User deleted!"
     redirect_to users_url
   end
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:name, :email, :phone, :address, :password,
                                    :password_confirmation)
     end
 
@@ -61,12 +61,12 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       unless current_user?(@user)
         flash[:warning] = "You're not enouch permission to edit an other account."
-        redirect_to root_path 
+        redirect_to root_path
       end
     end
 
     def admin_user
-      unless current_user_admin? 
+      unless current_user_admin?
         flash[:danger] = "You're not admin user!"
         redirect_to root_url
       end
